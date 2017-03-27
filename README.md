8d24ad6bb34e0229c0c15e35c155003a7cb1150d
Ben Mercer <ben@henroben.co.uk>
2017-03-27 14:40:27 +0100

    Babel and css file loaders added

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Starter Repo for a Webpack course on Udemy
 
 ```
 npm install
+npm run build
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,18 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+          use: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/ // exclude node modules as trust that all are ES5 already
+      },
+      {
+          use: ['style-loader', 'css-loader'],
+          test: /\.css$/
+      }
+    ]
   }
 };
